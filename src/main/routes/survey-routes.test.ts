28a9733acbb4f3aed423fa093faf701782908aeb
Felipe Pereira <feliperrpereira@gmail.com>
2023-01-10 16:50:23 -0300

    test: ensure Survey route return 204 on add survey success

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,39 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+import env from '../config/env'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    return await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('survey')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should be return 204 on add survey sucess', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
